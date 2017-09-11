c931e2e153098ee659e3c8cc15e2d89195b37b9d
jacqueselliott <jacques.g.elliott@gmail.com>
2017-09-11 15:58:38 +0100

    Create descriptor.hacks.pb.go

---
+++ protoc-gen-go/descriptor/descriptor.hacks.pb.go
@@ -0,0 +1,8 @@
+package descriptor
+
+func (this *FileDescriptorSet) LogFields() map[string]string {
+	return map[string]string{}
+}
+
+func (this *FileDescriptorSet) ExtractRequestFields(dst map[string]interface{}) {
+}
